a988397ba1c267a7006a019e66b479df8845b3e2
Samadi van Koten <samadi@vktec.org.uk>
2021-01-08 05:09:28 +0000

    Switch from go-gl/gl to vktec/gll

--- debug.go
+++ debug.go
@@ -3,65 +3,64 @@ package gldebug
 import (
 	"fmt"
 	"log"
-	"unsafe"
 
-	"github.com/go-gl/gl/v2.1/gl"
+	"github.com/vktec/gll"
 )
 
 // MessageCallback outputs an OpenGL debug message.
-// It is designed for use with gl.DebugMessageCallback.
-func MessageCallback(source, gltype, id, severity uint32, length int32, message string, userParam unsafe.Pointer) {
+// It is designed for use with glDebugMessageCallback.
+func MessageCallback(source, type_, id, severity uint32, length int32, message string) {
 	var sourceStr, typeStr, severityStr string
 
 	switch source {
-	case gl.DEBUG_SOURCE_API:
+	case gll.DEBUG_SOURCE_API:
 		sourceStr = "API"
-	case gl.DEBUG_SOURCE_WINDOW_SYSTEM:
+	case gll.DEBUG_SOURCE_WINDOW_SYSTEM:
 		sourceStr = "Window System"
-	case gl.DEBUG_SOURCE_SHADER_COMPILER:
+	case gll.DEBUG_SOURCE_SHADER_COMPILER:
 		sourceStr = "Shader Compiler"
-	case gl.DEBUG_SOURCE_THIRD_PARTY:
+	case gll.DEBUG_SOURCE_THIRD_PARTY:
 		sourceStr = "Third Party"
-	case gl.DEBUG_SOURCE_APPLICATION:
+	case gll.DEBUG_SOURCE_APPLICATION:
 		sourceStr = "Application"
-	case gl.DEBUG_SOURCE_OTHER:
+	case gll.DEBUG_SOURCE_OTHER:
 		sourceStr = "Other"
 	}
 
-	switch gltype {
-	case gl.DEBUG_TYPE_ERROR:
+	switch type_ {
+	case gll.DEBUG_TYPE_ERROR:
 		typeStr = "Error"
-	case gl.DEBUG_TYPE_DEPRECATED_BEHAVIOR:
+	case gll.DEBUG_TYPE_DEPRECATED_BEHAVIOR:
 		typeStr = "Deprecated Behaviour"
-	case gl.DEBUG_TYPE_UNDEFINED_BEHAVIOR:
+	case gll.DEBUG_TYPE_UNDEFINED_BEHAVIOR:
 		typeStr = "Undefined Behaviour"
-	case gl.DEBUG_TYPE_PORTABILITY:
+	case gll.DEBUG_TYPE_PORTABILITY:
 		typeStr = "Portability"
-	case gl.DEBUG_TYPE_PERFORMANCE:
+	case gll.DEBUG_TYPE_PERFORMANCE:
 		typeStr = "Performance"
-	case gl.DEBUG_TYPE_MARKER:
+	case gll.DEBUG_TYPE_MARKER:
 		typeStr = "Marker"
-	case gl.DEBUG_TYPE_PUSH_GROUP:
+	case gll.DEBUG_TYPE_PUSH_GROUP:
 		typeStr = "Push Group"
-	case gl.DEBUG_TYPE_POP_GROUP:
+	case gll.DEBUG_TYPE_POP_GROUP:
 		typeStr = "Pop Group"
-	case gl.DEBUG_TYPE_OTHER:
+	case gll.DEBUG_TYPE_OTHER:
 		typeStr = "Other"
 	}
 
 	switch severity {
-	case gl.DEBUG_SEVERITY_HIGH:
+	case gll.DEBUG_SEVERITY_HIGH:
 		severityStr = "High"
-	case gl.DEBUG_SEVERITY_MEDIUM:
+	case gll.DEBUG_SEVERITY_MEDIUM:
 		severityStr = "Medium"
-	case gl.DEBUG_SEVERITY_LOW:
+	case gll.DEBUG_SEVERITY_LOW:
 		severityStr = "Low"
-	case gl.DEBUG_SEVERITY_NOTIFICATION:
+	case gll.DEBUG_SEVERITY_NOTIFICATION:
 		severityStr = "Notification"
 	}
 
 	msg := fmt.Sprintf("(%d) source: %s, type: %s, severity: %s, message: %s", id, sourceStr, typeStr, severityStr, message)
-	if severity == gl.DEBUG_SEVERITY_HIGH {
+	if severity == gll.DEBUG_SEVERITY_HIGH {
 		panic(msg)
 	} else {
 		log.Println("GL debug:", msg)
